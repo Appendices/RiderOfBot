837b36a1be5e037667289146c0e9a7581aa4f6f2
Appendices <random.appendices@gmail.com>
2018-05-25 14:40:16 -0400

    Searches and outputs fixed. Added lewd and non-lewd only searches.

--- bot.js
+++ bot.js
@@ -7,6 +7,16 @@ const search = require("./search.js");
 //Initialize Discord Bot
 const bot = new Discord.Client();
 
+function menu(){
+    var menu = "**~help**\t|\tDisplays the menu\n" +
+                "**~succ**\t|\tSucks your dick and calls you gay\n" +
+                "**~TRS**\t|\tThe Redstone Scientist\n" +
+                "**~lewd**\t[tag]\t|\tLewd search\n" +
+                "**~safe**\t[tag]\t|\tSafe seach\n";
+                "**~search**\t[tag] [tag]\t|\tAny search\n";
+    return menu;
+}
+
 //Script started
 bot.on("ready", () => {
     console.log("Logged in to: " + bot.guilds.array());
@@ -18,13 +28,12 @@ bot.on("message", message => {
     //Listens for commands starting with ~'
     if(message.content.substring(0, 1) == "~"){
         var args = message.content.substring(1).split(" ");
+        var tag = "";
+        if(args[1]) tag = args[1];
 
         switch(args[0].toLowerCase()){
             case "help": //~help
-                message.channel.send("~succ\n" + 
-                                    "~TRS\n" +
-                                    "~search [tag]"
-                                    );
+                message.channel.send(menu());
                 break;
             case "succ": //~succ
                 message.channel.send("*succ* *succ* You gay.");
@@ -35,21 +44,20 @@ bot.on("message", message => {
             case "trs?": //~TRS?
                 message.channel.send("https://locker.ifttt.com/v2/12046472/1524033255062-e35a12156bb07745/478242495-hqoa6ui.jpg?sharing_key=b62df0377d751df02f73502e3d4cc9b5");
                 break;
-            case "search": //~search [tag]
-                if(args.length < 2){
-                    message.channel.send("Search term required, Mastah.");
-                    break;
-                }
-                var tag = args[1]; //combine args into compound tag
-                for(i = 2; i < args.length; i++){
-                    tag = tag + "+" + args[i];
-                }
+            case "lewd": //~lewd [tag]
+                search.explDanbooru(tag)
+                    .then(imgurl => message.channel.send(imgurl))
+                    .catch(err => message.channel.send("I don't feel so good, Mastah."));
+                break;
+            case "safe": //~safe [tag]
+                search.safeDanbooru(tag)
+                    .then(imgurl => message.channel.send(imgurl))
+                    .catch(err => message.channel.send("I don't feel so good, Mastah."));
+                break;
+            case "search": //~search [tag] [tag]
                 search.danbooru(tag)
                     .then(imgurl => message.channel.send(imgurl))
-                    .catch(err => {
-                        console.error(err);
-                        message.channel.send("I don't feel so good, Mastah.");
-                    });
+                    .catch(err => message.channel.send("I don't feel so good, Mastah."));
                 break;
             //Add more commands here
         }
--- search.js
+++ search.js
@@ -5,38 +5,50 @@ const request = require("tinyreq");
 module.exports = {
     danbooru: function(tag){
         return new Promise((resolve, reject) => {
-            var url = "https://danbooru.donmai.us/posts?page=1&tags=" + tag;
+            var url = "https://danbooru.donmai.us/posts?tags=" + tag;
             request(url, (err, body) => {
-                if(err) reject(err);
+                if(err) return reject(err);
                 else{
+                    console.log(url);
+                    if(body.includes("You cannot search for more than 2 tags at a time")){
+                        console.log("Too many tags.");
+                        return reject(err);
+                    }
+                    if(body.includes("Nobody here but us chickens!")){
+                        console.log("Empty search.");
+                        return reject(err);
+                    }
                     //Using known strings in html to find number of pages
                     var paginator = body.indexOf("<div class=\"paginator\"");
                     var arrow = body.indexOf("</a></li><li class='arrow'>");
-                    var greaterthan = body.substring(paginator, pages).lastIndexOf(">");
-                    var pages = body.substring(paginator + greaterthan + 1, pages);
+                    var greaterthan = body.substring(paginator, arrow).lastIndexOf(">");
+                    var pages = body.substring(paginator + greaterthan + 1, arrow);
                     //Select random page and construct new URL
                     url = "https://danbooru.donmai.us/posts?page=" + (Math.random() * pages | 0 + 1) + "&tags=" + tag;
+                    if(isNaN(pages)) pages = 1; //Single page won't have arrow in the same way
                     console.log("pages: " + pages + " | " + url);
                     request(url, (err, body) => {
-                        if(err) reject(err);
+                        if(err) return reject(err);
                         else{
                             //Using known strings in html to find image direct links
                             var posts = body.indexOf("<div style=\"overflow: hidden;\">");
                             var articles = body.substring(posts, body.substring(posts).indexOf("</div>") + posts).split("<article");
-                            if(articles.length < 2){
-                                reject("Empty search.");
-                                return; //kills search
-                            }
                             var article = (Math.random() * (articles.length - 1) | 0) + 1;
                             article = articles[article];
                             var imgurl = article.substring(article.indexOf("data-file-url=") + 15);
                             imgurl = imgurl.substring(0, imgurl.indexOf("\""));
-                            console.log("images " + (articles - 1) + " | " + imgurl);
-                            resolve(imgurl);
+                            console.log("images: " + (articles.length - 1) + " | " + imgurl);
+                            return resolve(imgurl);
                         }
                     });
                 }
             });
         });
+    },
+    explDanbooru: function(tag){
+        return this.danbooru("rating%3Ae+" + tag);
+    },
+    safeDanbooru: function(tag){
+        return this.danbooru("rating%3As+" + tag);
     }
 }
